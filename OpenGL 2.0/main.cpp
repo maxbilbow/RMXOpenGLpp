aebe6bc863f3d0b75aaf1f0de81fa9a52fe96e8e
Max Bilbow <Max@RattleAir-1.local>
2015-01-23 15:56:56 +0000

    Initial Commit

---
+++ OpenGL 2.0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL 2.0
+//
+//  Created by Max Bilbow on 23/01/2015.
+//  Copyright (c) 2015 Rattle Media Ltd. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
